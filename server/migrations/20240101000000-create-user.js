21390af970b8aaa115d104c4c4880798997f8935
Twoje Imię i Nazwisko <twoj.email@example.com>
2025-10-30 07:53:31 +0000

    user1

--- server/migrations/20240101000000-create-user.js
+++ server/migrations/20240101000000-create-user.js
@@ -32,10 +32,6 @@ module.exports = {
         allowNull: false,
         defaultValue: 'user'
       },
-      refresh_token: {
-        type: Sequelize.STRING(512), // Zgodnie z modelem User
-        allowNull: true
-      },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
--- server/migrations/20251029202242-initial-schema-creation.js
+++ server/migrations/20251029202242-initial-schema-creation.js
@@ -2,21 +2,16 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    // Dodajemy kolumnę 'refresh_token' do tabeli 'users'
+    await queryInterface.addColumn('users', 'refresh_token', {
+      type: Sequelize.STRING(512),
+      allowNull: true,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    // Usuwamy kolumnę 'refresh_token' w przypadku cofania migracji
+    await queryInterface.removeColumn('users', 'refresh_token');
   }
 };
